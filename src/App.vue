5ed34d316b0f881e7311e124b4ae958388db21cc
X1 Carbon <yjl9903@vip.qq.com>
2020-07-11 01:41:23 +0800

    add: footer

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
       <b-loading :active.sync="$root.loading"></b-loading>
       <router-view v-if="!$root.loading" />
     </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Codeforces Any</strong> by <a href="https://xlor.cn">XLor</a>.
+          The source code is hosted on
+          <a href="https://github.com/yjl9903/CodeforcesAny">Github</a> and is
+          licensed
+          <a href="https://github.com/yjl9903/CodeforcesAny/blob/master/LICENSE"
+            >MIT</a
+          >.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -28,6 +41,8 @@ $primary-invert: findColorInvert($primary);
 $navbar-padding-vertical: 0.5rem;
 
 $section-padding: 2rem 2rem;
+
+$footer-padding: 2rem 1.5rem 2rem;
 
 @import '~bulma';
 @import '~buefy/src/scss/buefy';
