706a0f550f788976e3b78883b6cb56248b7f0481
XLor <yjl9903@vip.qq.com>
2020-11-30 01:22:00 +0800

    update: dep

--- types/VToolTip.d.ts
+++
@@ -1,9 +0,0 @@
-import Vue, { VueConstructor, DirectiveOptions, PluginFunction } from 'vue';
-
-declare module 'v-tooltip' {
-  const vToolTip: PluginFunction<any>;
-  export default vToolTip;
-  export const VPopover: VueConstructor<Vue>;
-  export const VClosePopover: DirectiveOptions;
-  export const VTooltip: DirectiveOptions;
-}
